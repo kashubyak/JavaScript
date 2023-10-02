27ca1b862028863313a6540a1762cf5a88ce8ba9
Kashubyak Mykila <kashubyak514@gmail.com>
2023-10-02 22:05:58 +0300

    Lab1 finished, Lab2 started

--- lab2/js/script.js
+++ lab2/js/script.js
@@ -49,52 +49,6 @@ function task3() {
 	placeholderCursor();
 };
 
-
-function task4() {
-	const item4__result = document.getElementById('item4__result');
-	const task4_result_but = document.getElementById('task4_result_but');
-	function calculateEquations() {
-		const x = parseFloat(document.getElementById("task4x").value);
-		const y = parseFloat(document.getElementById("task4y").value);
-		let result;
-		if (x * y != 0) {
-			result = x + y;
-		} else if (x ** 3 > 100) {
-			result = x;
-		} else {
-			result = x ** 3 + y;
-		}
-		item4__result.innerHTML = `Результат обчислення: ${result}`;
-	}
-	task4_result_but.addEventListener('click', calculateEquations);
-};
-function task5() {
-	const item5__result = document.getElementById('item5__result');
-	const task5_result_but = document.getElementById('task5_result_but');
-
-	task5_result_but.addEventListener('click', function calculateSum() {
-		const x = parseFloat(document.getElementById('task5x').value);
-		const y = parseFloat(document.getElementById('task5y').value);
-
-		function factorial(n) {
-			let fact = 1;
-			for (let i = 2; i <= n; i++) {
-				fact *= i;
-			}
-			return fact;
-		}
-
-		let sum = 0;
-		for (let i = 2; i <= 6; i++) {
-			sum += Math.pow(-1, i) * (1 + 2 * x * y - Math.pow(x, 2)) / factorial(i + 1);
-		}
-		
-		item5__result.innerHTML = `Результат обчислення: ${sum}`;
-	});
-}
-
-
-
 function task6() {
 	const continents = ['Африка', 'Азія', 'Європа', 'Північна Америка', 'Південна Америка', 'Австралія', 'Антарктида'];
 	const populationDensity = [36, 149, 73, 22, 24, 3, 0.0003];
@@ -133,6 +87,5 @@ function task6() {
 task1();
 task2();
 task3();
-task4();
-task5();
+
 task6();
